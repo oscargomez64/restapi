fd70dd78b32c32cae5b92d6f75d978f5a4a1e245
Oscar Gomez <oscarruiz6448@hotmail.com>
2025-02-27 21:59:22 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer(function(req, res) {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    try {
+        const html = fs.readFileSync('index.html');
+        res.write(html);
+        res.end();
+    } catch(e) {
+        res.statusCode = 404;
+        res.write("Bad request");
+        console.log(e);
+        res.end();
+    }
+})
+
+const port = 3001;
+
+server.listen(port, function() {
+    console.log("El servidor se está ejecutando en " + port);
+});
